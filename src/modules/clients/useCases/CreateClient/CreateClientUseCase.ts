7067e8219b390e9bd7c3d468cdc2212ff73fbd8b
Anna Maria <aannamariabr@outlook.com>
2022-12-22 11:15:36 -0300

    Cadastro de cliente

---
+++ src/modules/clients/useCases/CreateClient/CreateClientUseCase.ts
@@ -0,0 +1,32 @@
+import { hash } from "bcrypt";
+import { prisma } from "../../../../database/prismaClient";
+
+interface ICreateClientDTO {
+  username: string;
+  password: string;
+}
+
+export class CreateClientUseCase {
+  async execute({ username, password }: ICreateClientDTO) {
+    const clientExists = await prisma.clients.findFirst({
+      where: {
+        username
+      }
+    })
+
+    if (clientExists) {
+      throw new Error("Client already exists")
+    }
+
+    const hashPassword = await hash(password, 10)
+
+    const client = await prisma.clients.create({
+      data: {
+        username,
+        password: hashPassword
+      }
+    })
+
+    return client
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express'
+import { routes } from './routes'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' })
-})
+app.use(express.json())
+
+app.use(routes)
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000 🚀')
